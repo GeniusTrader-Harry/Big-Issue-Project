eb4de66027a8405a4df2b930ecc677f45bc4b398
2267949156@qq.com <2267949156@qq.com>
2025-11-15 01:35:40 +0000

    changed corresponding address

--- Data_Wrangling.R
+++ Data_Wrangling.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 
-Brazil_PreTaxIncome_Top1Percent <- read.csv("Brazil_PreTaxIncome_Top1%.csv",
+
+## Wrangling Income Top 1 Percent
+
+Brazil_PreTaxIncome_Top1Percent <- read.csv("Brazil/Brazil_PreTaxIncome_Top1%.csv",
                                             sep = ";",
                                             skip = 1,
                                             header = FALSE,
@@ -11,7 +14,68 @@ colnames(Brazil_PreTaxIncome_Top1Percent) <- c("Country","Percentile","aa","Year
 Brazil_PreTaxIncome_Top1Percent <- Brazil_PreTaxIncome_Top1Percent %>% 
   select(Country,Percentile,Year,`% of Total`)
 
-Brazil_PreTaxIncome_Top1Percent$Percentile <- "Top 1% Share"
+Brazil_PreTaxIncome_Top1Percent$Percentile <- "Pre Tax Income Top 1% Share"
 Brazil_PreTaxIncome_Top1Percent$Year <- as.numeric(Brazil_PreTaxIncome_Top1Percent$Year)
-Brazil_PreTaxIncome_Top1Percent$`% of Total` <- as.numeric(Brazil_PreTaxIncome_Top1Percent$`% of Total`
+Brazil_PreTaxIncome_Top1Percent$`% of Total` <- as.numeric(Brazil_PreTaxIncome_Top1Percent$`% of Total`)
+
+Brazil_PreTaxIncome_Top1Percent <- Brazil_PreTaxIncome_Top1Percent[182:nrow(Brazil_PreTaxIncome_Top1Percent), ]
+
+
+## Wrangling Income Top 10 Percent
                                                            
+Brazil_PreTaxIncome_Top10Percent <- read.csv("Brazil/Brazil_PreTaxIncome_Top10%.csv",
+                                             sep = ";",
+                                             skip = 1,
+                                             header = FALSE,
+                                             quote = "\"")
+
+colnames(Brazil_PreTaxIncome_Top10Percent) <- c("Country","Percentile","aa","Year","% of Total")
+
+Brazil_PreTaxIncome_Top10Percent <- Brazil_PreTaxIncome_Top10Percent %>% 
+  select(Country,Percentile,Year,`% of Total`)
+
+Brazil_PreTaxIncome_Top10Percent$Percentile <- "Pre Tax Income Top 10% Share"
+Brazil_PreTaxIncome_Top10Percent$Year <- as.numeric(Brazil_PreTaxIncome_Top10Percent$Year)
+Brazil_PreTaxIncome_Top10Percent$`% of Total` <- as.numeric(Brazil_PreTaxIncome_Top10Percent$`% of Total`)
+
+Brazil_PreTaxIncome_Top10Percent <- Brazil_PreTaxIncome_Top10Percent[182:nrow(Brazil_PreTaxIncome_Top10Percent), ]
+
+## Wrangling Wealth Top 1 Percent
+
+
+Brazil_NetPersonalWealth_Top1Percent <- read.csv("Brazil/Brazil_PreTaxIncome_Top1%.csv",
+                                                 sep = ";",
+                                                 skip = 1,
+                                                 header = FALSE,
+                                                 quote = "\"")
+colnames(Brazil_NetPersonalWealth_Top1Percent) <- c("Country","Percentile","aa","Year","% of Total")
+
+Brazil_NetPersonalWealth_Top1Percent <- Brazil_NetPersonalWealth_Top1Percent %>% 
+  select(Country,Percentile,Year,`% of Total`)
+
+Brazil_NetPersonalWealth_Top1Percent$Percentile <- "Net Personal Wealth Top 1% Share"
+Brazil_NetPersonalWealth_Top1Percent$Year <- as.numeric(Brazil_NetPersonalWealth_Top1Percent$Year)
+Brazil_NetPersonalWealth_Top1Percent$`% of Total` <- as.numeric(Brazil_NetPersonalWealth_Top1Percent$`% of Total`)
+
+
+Brazil_NetPersonalWealth_Top1Percent <- Brazil_NetPersonalWealth_Top1Percent[182:nrow(Brazil_NetPersonalWealth_Top1Percent), ]
+
+
+
+## Wrangling Wealth Top 10 Percent
+
+Brazil_NetPersonalWealth_Top10Percent <- read.csv("Brazil/Brazil_PreTaxIncome_Top10%.csv",
+                                                 sep = ";",
+                                                 skip = 1,
+                                                 header = FALSE,
+                                                 quote = "\"")
+colnames(Brazil_NetPersonalWealth_Top10Percent) <- c("Country","Percentile","aa","Year","% of Total")
+
+Brazil_NetPersonalWealth_Top10Percent <- Brazil_NetPersonalWealth_Top10Percent %>% 
+  select(Country,Percentile,Year,`% of Total`)
+
+Brazil_NetPersonalWealth_Top10Percent$Percentile <- "Net Personal Wealth Top 10% Share"
+Brazil_NetPersonalWealth_Top10Percent$Year <- as.numeric(Brazil_NetPersonalWealth_Top10Percent$Year)
+Brazil_NetPersonalWealth_Top10Percent$`% of Total` <- as.numeric(Brazil_NetPersonalWealth_Top10Percent$`% of Total`)
+
+Brazil_NetPersonalWealth_Top10Percent <- Brazil_NetPersonalWealth_Top10Percent[182:nrow(Brazil_NetPersonalWealth_Top10Percent), ]
